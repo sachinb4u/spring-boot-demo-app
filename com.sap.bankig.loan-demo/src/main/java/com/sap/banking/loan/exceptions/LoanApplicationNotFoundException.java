7346dd865cad1d7d000019e371747e806fe7f354
Sachin Bhosale <sachinb4u@gmail.com>
2017-12-06 15:12:51 +0530

    Base business exception added

--- com.sap.bankig.loan-demo/src/main/java/com/sap/banking/loan/exceptions/LoanApplicationNotFoundException.java
+++ com.sap.bankig.loan-demo/src/main/java/com/sap/banking/loan/exceptions/LoanApplicationNotFoundException.java
@@ -13,7 +13,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
  *
  */
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class LoanApplicationNotFoundException extends RuntimeException {
+public class LoanApplicationNotFoundException extends BusinessException {
 
 	private static final long serialVersionUID = 6421372506198842581L;
 
--- com.sap.bankig.loan-demo/src/main/java/com/sap/banking/loan/exceptions/UnauthorizedAccessException.java
+++ com.sap.bankig.loan-demo/src/main/java/com/sap/banking/loan/exceptions/UnauthorizedAccessException.java
@@ -1,6 +1,6 @@
 package com.sap.banking.loan.exceptions;
 
-public class UnauthorizedAccessException extends RuntimeException {
+public class UnauthorizedAccessException extends BusinessException {
 
 	private static final long serialVersionUID = -9060884606123616015L;
 	
