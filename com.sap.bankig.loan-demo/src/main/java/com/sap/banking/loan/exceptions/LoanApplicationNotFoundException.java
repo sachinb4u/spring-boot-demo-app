36ce95d99fcf6c2b778382f13b68c97e6b380d66
Sachin Bhosale <sachinb4u@gmail.com>
2017-12-06 14:33:01 +0530

    Response status exception handler

--- com.sap.bankig.loan-demo/src/main/java/com/sap/banking/loan/exceptions/LoanApplicationNotFoundException.java
+++ com.sap.bankig.loan-demo/src/main/java/com/sap/banking/loan/exceptions/LoanApplicationNotFoundException.java
@@ -2,12 +2,21 @@ package com.sap.banking.loan.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+/**
+ * {@link ResponseStatus} annotation is handled by
+ * {@link ResponseEntityExceptionHandler} to set the status code if this
+ * exception is thrown
+ * 
+ * @author I313873
+ *
+ */
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class LoanApplicationNotFoundException extends RuntimeException {
 
 	private static final long serialVersionUID = 6421372506198842581L;
-	
+
 	public LoanApplicationNotFoundException(String appId) {
 		super("Loan Application Not Found with id " + appId);
 	}
