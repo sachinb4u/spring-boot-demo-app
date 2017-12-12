ce1ce784bea09b2b960ff0177a0cce3eceac625d
Sachin Bhosale <sachinb4u@gmail.com>
2017-12-12 17:39:27 +0530

    Persistence - Spring Data JPA repository added

---
+++ com.sap.bankig.loan-demo/src/main/java/com/sap/banking/loan/persistence/LoanApplicationRepository.java
@@ -0,0 +1,9 @@
+package com.sap.banking.loan.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sap.banking.loan.beans.LoanApplication;
+
+public interface LoanApplicationRepository extends JpaRepository<LoanApplication, String> {
+
+}
