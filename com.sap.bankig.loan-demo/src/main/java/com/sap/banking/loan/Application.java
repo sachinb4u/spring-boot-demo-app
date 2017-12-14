7bbcfb38d0eb57d454e7b76dbc9d12682cbd7e5a
Sachin Bhosale <sachinb4u@gmail.com>
2017-12-14 15:43:08 +0530

    CloudFoundry deployment support added with service scan and build dependencies

--- com.sap.bankig.loan-demo/src/main/java/com/sap/banking/loan/Application.java
+++ com.sap.bankig.loan-demo/src/main/java/com/sap/banking/loan/Application.java
@@ -2,8 +2,12 @@ package com.sap.banking.loan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.java.ServiceScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@Configuration
+@ServiceScan
 @EnableJpaRepositories
 @SpringBootApplication
 public class Application {
