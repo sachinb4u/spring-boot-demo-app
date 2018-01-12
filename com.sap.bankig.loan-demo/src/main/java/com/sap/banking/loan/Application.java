22cf8ba6b6e2f36a3a58299216d0f0d4105cd853
Sachin Bhosale <sachinb4u@gmail.com>
2018-01-12 11:20:09 +0530

    Build fix - cloud configuration (service scan) applied for cloud profile and removed for local

--- com.sap.bankig.loan-demo/src/main/java/com/sap/banking/loan/Application.java
+++ com.sap.bankig.loan-demo/src/main/java/com/sap/banking/loan/Application.java
@@ -4,15 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.java.ServiceScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@Configuration
-@ServiceScan
 @EnableJpaRepositories
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Profile({ "cloud" })
+	@Configuration
+	@ServiceScan
+	public static class CloudConfiguration {
+
 	}
 }
