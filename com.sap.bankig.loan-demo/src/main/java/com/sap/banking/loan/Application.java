2b7def2313c7f0ca8c475cfb1912a1b22eaac765
Sachin Bhosale <sachinb4u@gmail.com>
2018-01-12 13:04:41 +0530

    Refactoring to move CloudConfiguration to separate package

--- com.sap.bankig.loan-demo/src/main/java/com/sap/banking/loan/Application.java
+++ com.sap.bankig.loan-demo/src/main/java/com/sap/banking/loan/Application.java
@@ -2,9 +2,6 @@ package com.sap.banking.loan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.java.ServiceScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories
@@ -13,12 +10,5 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	@Profile({ "cloud" })
-	@Configuration
-	@ServiceScan
-	public static class CloudConfiguration {
-
 	}
 }
